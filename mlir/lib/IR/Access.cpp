bc4f92e7ed293f3ebb37f3f717bcd5531a4e6855
lorenzo chelini <l.chelini@tue.nl>
2020-02-17 16:52:15 +0100

    [MLIR] Introduce placeholder (m_Placholder)
    Introduce data structures for placeholder. It is a centralized matching around
    the matching context class, which has a "global" view on what is going on
    during the matching phase. When a placeholder is instatiated it is also
    registered to the context. During the matching a placeholder asks the context
    if he can match the current Value.

---
+++ mlir/lib/IR/Access.cpp
@@ -0,0 +1,80 @@
+#include "mlir/IR/Access.h"
+
+using namespace mlir;
+using namespace matchers;
+using namespace details;
+
+LogicalResult MatchingContext::getValueForId(size_t placeholderId,
+                                             Value &value) const {
+  auto it = placeholderMap_.find(placeholderId);
+  if (it == placeholderMap_.end())
+    return failure();
+  value = it->second;
+  return success();
+}
+
+MatchingContext *&m_Placeholder::context() {
+  thread_local MatchingContext *context = nullptr;
+  return context;
+}
+
+void MatchingContext::registerPlaceholder(size_t placeholderId) {
+  placeholderMap_.insert({placeholderId, nullptr});
+}
+
+void MatchingContext::dump() {
+  llvm::outs() << "dumping current context..\n";
+  for (const auto &it : placeholderMap_) {
+    llvm::outs() << "first: " << it.first << "\n";
+    if (it.second) {
+      llvm::outs() << "second: " << &it.second << "\n";
+    } else
+      llvm::outs() << "second: nullptr"
+                   << "\n";
+    llvm::outs() << "---\n";
+  }
+}
+
+LogicalResult MatchingContext::assignToPlaceholder(Value &val,
+                                                   size_t placeholderId) {
+  auto it = placeholderMap_.find(placeholderId);
+  assert(it != placeholderMap_.end() && "placeholder not registered");
+
+  if (!it->second) {
+    it->second = val;
+    return success();
+  }
+  // Placeholder with id "placeholderId" assigned to a
+  // given value "val". If we want to assing another value
+  // to the same id "placeholderId", the match must exit false.
+  if (it->second != val)
+    return failure();
+  return success();
+}
+
+void m_Placeholder::dump() {
+  llvm::outs() << "dumping placeholder..\n";
+  llvm::outs() << id_;
+}
+
+AffinePattern m_Placeholder::buildDefaultAffinePattern() {
+  assert(context() != nullptr && "expect initialized context");
+  auto ctx = context()->getContext();
+  assert(ctx != nullptr && "expected initialized context");
+  return details::AffinePattern(ctx);
+}
+
+m_Placeholder::m_Placeholder()
+    : pattern_(buildDefaultAffinePattern()), id_(nextId_++) {
+  assert(context() != nullptr && "expect initialized context");
+  auto ctx = context();
+  ctx->registerPlaceholder(id_);
+}
+
+Value AccessPatternContext::operator[](const m_Placeholder &pl) const {
+  Value value = nullptr;
+  auto result = matchingContext_.getValueForId(pl.id_, value);
+  if (failed(result))
+    assert(0 && "placeholder not found");
+  return value;
+}
--- mlir/test/IR/test-matchers.mlir
+++ mlir/test/IR/test-matchers.mlir
@@ -41,3 +41,42 @@ func @test2(%a: f32) -> f32 {
 // CHECK-LABEL: test2
 //       CHECK:   Pattern add(add(a, constant), a) matched and bound constant to: 1.000000e+00
 //       CHECK:   Pattern add(add(a, constant), a) matched
+
+func @matmul(%A: memref<42x40xf32>, %B: memref<40x41xf32>, %C: memref<42x41xf32>) {
+  affine.for %i = 0 to 42 {
+    affine.for %j = 0 to 41 {
+      affine.for %k = 0 to 40 {
+        %0 = affine.load %A[%i, %k] : memref<42x40xf32>
+        %1 = affine.load %B[%k, %j] : memref<40x41xf32>
+        %2 = affine.load %C[%i, %j] : memref<42x41xf32>
+        %3 = mulf %0, %1 : f32
+        %4 = addf %2, %3 : f32
+        affine.store %4, %C[%i, %j] : memref<42x41xf32>
+      }
+    }
+  }
+  return
+}
+
+// CHECK-LABEL: matmul
+//       CHECK: Pattern add(C(i, j), mul(A(i, k), B(k, j))) matched 1 times
+
+func @matmulTransB(%A: memref<42x40xf32>, %B: memref<40x41xf32>, %C: memref<42x41xf32>) {
+  affine.for %i = 0 to 42 {
+    affine.for %j = 0 to 41 {
+      affine.for %k = 0 to 40 {
+        %0 = affine.load %A[%i, %k] : memref<42x40xf32>
+        %1 = affine.load %B[%j, %k] : memref<40x41xf32>
+        %2 = affine.load %C[%i, %j] : memref<42x41xf32>
+        %3 = mulf %0, %1 : f32
+        %4 = addf %2, %3 : f32
+        affine.store %4, %C[%i, %j] : memref<42x41xf32>
+      }
+    }
+  }
+  return
+}
+
+// CHECK-LABEL: matmulTransB
+//       CHECK: Pattern add(C(i, j), mul(A(i, k), B(k, j))) matched 0 times
+//       CHECK: Pattern add(C(i, j), mul(A(i, k), B(j, k))) matched 1 times
