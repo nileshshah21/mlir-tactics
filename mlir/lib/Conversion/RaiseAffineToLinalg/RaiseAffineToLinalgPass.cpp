4902d3301956c74d61f51fc2de12c8ec8edc62eb
lorenzo chelini <l.chelini@tue.nl>
2020-02-21 14:14:44 +0100

    [MLIR] Emit directly BLAS call in Matmul matcher
    Run also Clang-format.

--- mlir/lib/Conversion/RaiseAffineToLinalg/RaiseAffineToLinalgPass.cpp
+++ mlir/lib/Conversion/RaiseAffineToLinalg/RaiseAffineToLinalgPass.cpp
@@ -1,13 +1,14 @@
 #include "mlir/Conversion/RaiseAffineToLinalg/RaiseAffineToLinalgPass.h"
-#include "mlir/Dialect/Linalg/IR/LinalgOps.h"
-#include "mlir/Pass/Pass.h"
-#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Analysis/NestedMatcher.h"
 #include "mlir/Dialect/AffineOps/AffineOps.h"
+#include "mlir/Dialect/Linalg/EDSC/Intrinsics.h"
+#include "mlir/Dialect/Linalg/IR/LinalgOps.h"
+#include "mlir/Dialect/StandardOps/Ops.h"
 #include "mlir/IR/Access.h"
 #include "mlir/IR/Matchers.h"
-#include "mlir/Dialect/StandardOps/Ops.h"
-#include "mlir/Analysis/NestedMatcher.h"
-#include "mlir/Dialect/Linalg/EDSC/Intrinsics.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "llvm/Support/Debug.h"
 
 using namespace mlir;
 
@@ -17,6 +18,12 @@ class RaiseAffineToLinalgPass : public FunctionPass<RaiseAffineToLinalgPass> {
 };
 } // namespace
 
+#define DEBUG_TYPE "mlir-raise-to-affine"
+
+static llvm::cl::opt<bool>
+    clEmitCall("emit-blas-call", llvm::cl::desc("directly emit blas calls."),
+               llvm::cl::init(false));
+
 namespace {
 
 /// Class for matching a matmul op.
@@ -25,11 +32,12 @@ public:
   using OpRewritePattern<AffineForOp>::OpRewritePattern;
 
   PatternMatchResult matchBody(Region &body, Value i, Value j, Value k,
-                               Value &operandA, Value &operandB, Value &operandC) const {
+                               Value &operandA, Value &operandB,
+                               Value &operandC) const {
     using namespace mlir::matchers;
     {
       AccessPatternContext pctx(body.getContext());
-  
+
       auto _i = m_Placeholder();
       auto _j = m_Placeholder();
       auto _k = m_Placeholder();
@@ -45,28 +53,28 @@ public:
       // TODO: relax the order of operands and introduce
       // ArrayPlaceholder.
       auto matMulOp = m_Op<AddFOp>(c, m_Op<MulFOp>(a, b));
-      
+
       auto store = dyn_cast<AffineStoreOp>(*std::prev(body.front().end(), 2));
 
       if (!matchPattern(store, m_Op<AffineStoreOp>(_C({_i, _j}))))
         return matchFailure();
-      
+
       auto add =
-        dyn_cast_or_null<AddFOp>(store.getValueToStore().getDefiningOp());
+          dyn_cast_or_null<AddFOp>(store.getValueToStore().getDefiningOp());
       if (!add)
         return matchFailure();
-      
-      if (!matMulOp.match(add)) 
+
+      if (!matMulOp.match(add))
         return matchFailure();
-   
+
       // TODO: we may be lenient to operations without side-effects, but they
-      // should have been removed by DCE beforehand. 
+      // should have been removed by DCE beforehand.
       if (std::distance(body.front().begin(), body.front().end()) != 7)
         return matchFailure();
-  
+
       if ((i != pctx[_i]) || (j != pctx[_j]) || (k != pctx[_k]))
         return matchFailure();
-    
+
       operandA = pctx[_A];
       operandB = pctx[_B];
       operandC = pctx[_C];
@@ -74,16 +82,19 @@ public:
     return matchSuccess();
   }
 
-  PatternMatchResult matchAndRewriteNestedPattern(Operation *op, PatternRewriter &rewriter) const {
+  PatternMatchResult
+  matchAndRewriteNestedPattern(Operation *op, PatternRewriter &rewriter) const {
     Value operandA, operandB, operandC;
     auto body = [this, &operandA, &operandB, &operandC](Operation &op) -> bool {
-      auto loop = cast<AffineForOp>(op);  
+      auto loop = cast<AffineForOp>(op);
       Value k = loop.getInductionVar();
       auto parent = loop.getParentOfType<AffineForOp>();
       Value j = parent.getInductionVar();
       parent = parent.getParentOfType<AffineForOp>();
       Value i = parent.getInductionVar();
-      return matchBody(loop.getLoopBody(), i, j, k, operandA, operandB, operandC).hasValue();
+      return matchBody(loop.getLoopBody(), i, j, k, operandA, operandB,
+                       operandC)
+          .hasValue();
     };
 
     {
@@ -95,23 +106,43 @@ public:
       if (matches.empty())
         return matchFailure();
     }
-      
+
     if ((!operandA) || (!operandB) || (!operandC))
       return matchFailure();
-    
-    using namespace edsc;
-    using namespace edsc::ops;
-    OpBuilder builder(op->getContext());
-    SmallVector<Value, 3> values = {operandA, operandB, operandC};
-    ScopedContext scop(builder, op->getLoc());
-    auto matMulOp = linalg_matmul(makeValueHandles(values));
-    rewriter.clone(*matMulOp);
-    matMulOp->erase();
-    
+
+    LLVM_DEBUG(llvm::dbgs() << "Matched Matmul pattern!\n");
+
+    SmallVector<Value, 3> operands = {operandA, operandB, operandC};
+    if (clEmitCall) {
+      // emit blas call.
+      auto module = op->getParentOfType<ModuleOp>();
+      auto operandAType = operandA.getType();
+      auto operandBType = operandB.getType();
+      auto operandCType = operandC.getType();
+      SmallVector<Type, 4> inputTypes{operandAType, operandBType, operandCType};
+      auto libFnType = FunctionType::get(inputTypes, {}, rewriter.getContext());
+      OpBuilder::InsertionGuard guard(rewriter);
+      rewriter.setInsertionPoint(module.getBody(),
+                                 std::prev(module.getBody()->end()));
+      rewriter.create<FuncOp>(op->getLoc(), "myMatmul", libFnType,
+                              ArrayRef<NamedAttribute>{});
+      rewriter.setInsertionPoint(op);
+      rewriter.create<CallOp>(op->getLoc(), "myMatmul", ArrayRef<Type>{},
+                              operands);
+    } else {
+      // emit linalg.
+      using namespace edsc;
+      using namespace edsc::ops;
+      OpBuilder builder(op->getContext());
+      ScopedContext scop(builder, op->getLoc());
+      auto matMulOp = linalg_matmul(makeValueHandles(operands));
+      rewriter.clone(*matMulOp);
+      matMulOp->erase();
+    }
     rewriter.eraseOp(op);
     return matchSuccess();
   }
-  
+
   // main rewriting function.
   PatternMatchResult matchAndRewrite(AffineForOp op,
                                      PatternRewriter &rewriter) const override {
@@ -124,8 +155,8 @@ public:
 void RaiseAffineToLinalgPass::runOnFunction() {
 
   ConversionTarget target(getContext());
-  target.addLegalDialect<mlir::linalg::LinalgDialect>();
-  target.addLegalOp<ReturnOp, ModuleOp, ModuleTerminatorOp, FuncOp>();
+  target.addLegalDialect<mlir::linalg::LinalgDialect, StandardOpsDialect>();
+  target.addLegalOp<ReturnOp, AllocOp, ModuleOp, ModuleTerminatorOp, FuncOp>();
 
   OwningRewritePatternList patterns;
 
