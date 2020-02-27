2f5af86cee0214c151ab9a6be0e290109e8f5b25
lorenzo chelini <l.chelini@tue.nl>
2020-02-27 16:20:24 +0100

    [MLIR] Code refactor for access matchers (NFC)
    This revision performs some basic refactoring by eliminating redundant code.

--- mlir/include/mlir/IR/Access-inl.h
+++ mlir/include/mlir/IR/Access-inl.h
@@ -3,30 +3,9 @@ namespace mlir {
 
 namespace matchers {
 
-
-// FIXME: remove duplicate code.
 template <typename OpClass>
-bool op_load_store_matcher<OpClass>::matchLoadOp(AffineLoadOp &op) {
-  size_t dims = op.getAffineMap().getNumResults();
-  if (dims != placeholders_.size())
-    return false;
-  SmallVector<Value, 4> operands = op.getMapOperands();
-  for (size_t dim = 0; dim < dims; dim++) {
-    AffineExpr affine = op.getAffineMap().getResult(dim);
-    // check the affine expression.
-    if (placeholders_[dim].pattern_.expr_ != affine)
-      return false;
-    auto matchingContext = placeholders_[dim].context();
-    auto isValid = matchingContext->assignToPlaceholder(operands[dim],
-                                                        placeholders_[dim].id_);
-    if (failed(isValid))
-      return false;
-  }
-  return true;
-}
-
-template <typename OpClass>
-bool op_load_store_matcher<OpClass>::matchStoreOp(AffineStoreOp &op) {
+template <typename T>
+bool op_load_store_matcher<OpClass>::matchLoadOrStoreOp(T &op) {
   size_t dims = op.getAffineMap().getNumResults();
   if (dims != placeholders_.size())
     return false;
@@ -51,9 +30,9 @@ bool op_load_store_matcher<OpClass>::match(Operation *op) {
   if ((!op) || (!isa<OpClass>(op)))
     return false;
   if (auto loadOp = dyn_cast<AffineLoadOp>(op))
-    return matchLoadOp(loadOp);
+    return matchLoadOrStoreOp<AffineLoadOp>(loadOp);
   if (auto storeOp = dyn_cast<AffineStoreOp>(op))
-    return matchStoreOp(storeOp);
+    return matchLoadOrStoreOp<AffineStoreOp>(storeOp);
   assert(0 && "expect AffineStoreOp or AffineLoadOp");
   return false;
 }
