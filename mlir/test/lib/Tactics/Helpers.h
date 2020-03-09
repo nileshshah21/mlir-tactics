98cf6746d820410b9e29e473255ae265537faf97
lorenzo chelini <l.chelini@tue.nl>
2020-03-09 10:22:52 +0100

    [MLIR] Change function signature for transpose
    Change signature for BLAS transpose to be consistent with the reshape
    BLAS signature.

--- mlir/test/lib/Tactics/Helpers.h
+++ mlir/test/lib/Tactics/Helpers.h
@@ -30,10 +30,15 @@ enum class FUNCTION {
 std::string
 composeFunctionNameForTranspose(const llvm::ArrayRef<mlir::Type> &types) {
   std::string result = "transpose_";
-  auto TShape = types[0].dyn_cast<mlir::MemRefType>().getShape();
-  for (size_t i = 0; i < TShape.size() - 1; i++)
-    result += std::to_string(TShape[i]) + "x";
-  result += std::to_string(TShape[TShape.size() - 1]);
+  auto SShape = types[0].dyn_cast<mlir::MemRefType>().getShape();
+  auto DShape = types[1].dyn_cast<mlir::MemRefType>().getShape();
+  for (size_t i = 0; i < SShape.size() - 1; i++)
+    result += std::to_string(SShape[i]) + "x";
+  result += std::to_string(SShape[SShape.size() - 1]);
+  result += "_to_";
+  for (size_t i = 0; i < DShape.size() - 1; i++)
+    result += std::to_string(DShape[i]) + "x";
+  result += std::to_string(DShape[DShape.size() - 1]);
   return result;
 }
 
