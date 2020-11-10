03b4f8c25de44a221d6bbd5997d11d0652ae48fa
lorenzo chelini <l.chelini@icloud.com>
2020-11-10 17:12:10 +0100

    more bench utilities

--- mlir/benchmark_section5.2/run.sh
+++ mlir/benchmark_section5.2/run.sh
@@ -26,37 +26,34 @@ CFLAGS="-convert-linalg-to-affine-loops -lower-affine -convert-scf-to-std -conve
 RUNNER="mlir-cpu-runner -O3 -e main -entry-point-result=void -shared-libs=/llvm-project/build/lib/libmlir_test_cblas_interface.so,/llvm-project/build/lib/libmlir_runner_utils.so"
 
 for bm in "${targets[@]}"; do
-  echo $bm
   x=`echo $bm | sed -e 's/\.mlir$//g'`
   for i in 1 2 3 4 5; do
     t=`mlir-opt $CFLAGS $bm | $RUNNER`
     x="$x:$t"
   done
-  #echo "$x"
-done
+  echo "$x"
+done &> results_clang.txt
 
 echo "Running Linalg with tile factor of 32"
 CFLAGS="-test-tactics-linalg -convert-linalg-to-affine-loops -affine-loop-tile="tile-size=32" -lower-affine -convert-linalg-to-llvm -convert-scf-to-std -convert-std-to-llvm"
 
 for bm in "${targets[@]}"; do
-  echo $bm
   x=`echo $bm | sed -e 's/\.mlir$//g'`
   for i in 1 2 3 4 5; do
     t=`mlir-opt $CFLAGS $bm | $RUNNER`
     x="$x:$t"
   done
-  #echo "$x"
-done
+  echo "$x"
+done &> results_linalg.txt
 
 echo "Running BLAS"
 CFLAGS="-test-tactics-blas-cpu -convert-linalg-to-affine-loops -lower-affine -convert-linalg-to-llvm -convert-scf-to-std -convert-std-to-llvm"
 
 for bm in "${targets[@]}"; do
-  echo $bm
   x=`echo $bm | sed -e 's/\.mlir$//g'`
   for i in 1 2 3 4 5; do
     t=`mlir-opt $CFLAGS $bm | $RUNNER`
     x="$x:$t"
   done
-  #echo "$x"
-done
+  echo "$x"
+done &> results_blas.txt
