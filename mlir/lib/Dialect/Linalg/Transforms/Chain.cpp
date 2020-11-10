939d7f7da78a1a602b25927b50ef72817531f2fc
lorenzo chelini <l.chelini@icloud.com>
2020-11-10 13:10:03 +0100

    add missing matchers

--- mlir/lib/Dialect/Linalg/Transforms/Chain.cpp
+++ mlir/lib/Dialect/Linalg/Transforms/Chain.cpp
@@ -8,6 +8,10 @@
 
 #define DEBUG_TYPE "matmul-chain"
 
+static llvm::cl::opt<int64_t>
+    chainSize("chain-size", llvm::cl::init(0),
+              llvm::cl::desc("Select which matcher to use"));
+
 using namespace mlir;
 using namespace mlir::linalg;
 
@@ -123,16 +127,255 @@ static long getCurrentNumberOfScalarMults(SmallVector<Value, 8> values) {
   return res;
 }
 
-class ChainMatmulOp : public RewritePattern {
+// check if the current value is the output of
+// a single linalg.matmul operation.
+static bool isNotUsedInMatmulOp(mlir::Value value) {
+  if (!value)
+    return true;
+  auto op = value.getDefiningOp();
+  if (!op)
+    return true;
+  auto users = op->getUsers();
+  if (std::distance(users.begin(), users.end()) != 1)
+    return false;
+  for (auto user : users)
+    if (!isa<linalg::MatmulOp>(user))
+      return false;
+  return true;
+}
+
+class ChainSixMatmulOp : public RewritePattern {
 private:
   static long currentMin;
+  static bool alreadyReordered;
 
 public:
-  ChainMatmulOp(MLIRContext *context)
+  ChainSixMatmulOp(MLIRContext *context)
       : RewritePattern(MatmulOp::getOperationName(), 0, context) {}
 
   LogicalResult matchAndRewrite(Operation *op,
                                 PatternRewriter &rewriter) const override {
+
+    if (alreadyReordered)
+      return failure();
+
+    using mlir::matchers::m_Any;
+    using mlir::matchers::m_AnyCapture;
+    Value A1 = nullptr, A2 = nullptr, A3 = nullptr, A4 = nullptr, A5 = nullptr,
+          A6 = nullptr;
+    Value B1 = nullptr, B2 = nullptr, B3 = nullptr, B4 = nullptr;
+    Value alpha1 = nullptr, alpha2 = nullptr, alpha3 = nullptr,
+          alpha4 = nullptr, alpha5 = nullptr;
+    Value beta1 = nullptr, beta2 = nullptr, beta3 = nullptr, beta4 = nullptr,
+          beta5 = nullptr;
+    // clang-format off
+    auto matcher =
+      m_Op<linalg::MatmulOp>(m_AnyCapture(alpha1), m_AnyCapture(beta1), m_AnyCapture(A1), m_AnyCapture(A2),
+        m_Op<linalg::MatmulOp>(m_AnyCapture(alpha2), m_AnyCapture(beta2), m_AnyCapture(B1), m_AnyCapture(A3),
+          m_Op<linalg::MatmulOp>(m_AnyCapture(alpha3), m_AnyCapture(beta3), m_AnyCapture(B2), m_AnyCapture(A4),
+            m_Op<linalg::MatmulOp>(m_AnyCapture(alpha4), m_AnyCapture(beta4), m_AnyCapture(B3), m_AnyCapture(A5),
+              m_Op<linalg::MatmulOp>(m_AnyCapture(alpha5), m_AnyCapture(beta5), m_AnyCapture(B4), m_AnyCapture(A6),
+                m_Any([](mlir::Value v) { return isNotUsedInMatmulOp(v); }))))));
+    // clang-format on
+    if (!matcher.match(op))
+      return failure();
+
+    SmallVector<Value, 8> capturedAllocs = {A1, A2, A3, A4, A5, A6};
+    auto pVector = getPVector(capturedAllocs);
+    if (!pVector.size())
+      return failure();
+
+    const size_t n = pVector.size();
+    std::vector<std::vector<long>> m(
+        n, std::vector<long>(n, std::numeric_limits<long>::max()));
+    std::vector<std::vector<long>> s(
+        n, std::vector<long>(n, std::numeric_limits<long>::max()));
+    matrixChainOrder(pVector, m, s);
+    if (!m.size() || !s.size())
+      return failure();
+
+    auto currentNumberScalarsMults =
+        getCurrentNumberOfScalarMults({A1, A2, B1, A3, B2, A4, B3, A5, B4, A6});
+
+    LLVM_DEBUG(llvm::dbgs()
+               << "Min number scalar multiplications: " << m[1][n - 1] << "\n");
+    LLVM_DEBUG(llvm::dbgs() << "Current number scalar multiplications: "
+                            << currentNumberScalarsMults << "\n");
+    LLVM_DEBUG(printOptimalParens(s, 1, 6));
+    LLVM_DEBUG(llvm::dbgs() << "\n\n");
+    LLVM_DEBUG(llvm::dbgs() << "Current Min: " << currentMin << "\n");
+
+    if (currentNumberScalarsMults == currentMin) {
+      return failure();
+    }
+    currentMin = m[1][n - 1];
+
+    SmallVector<Operation *, 8> matmuls = {};
+    getMatmuls(op, matmuls);
+    if (matmuls.size() != 5) {
+      LLVM_DEBUG(llvm::dbgs() << "Expect 5 matmul only");
+      return failure();
+    }
+
+    SmallVector<Operation *, 8> allocs = {};
+    getDefiningOpForMatmulOutputBuffer(matmuls, allocs);
+    if (!llvm::all_of(allocs, [](Operation *alloc) {
+          return dyn_cast_or_null<AllocOp>(alloc);
+        })) {
+      LLVM_DEBUG(llvm::dbgs() << "Expect only alloc operations as defining op "
+                                 "for the matmul output buffer\n");
+      return failure();
+    }
+    auto loc = op->getLoc();
+    rewriter.setInsertionPoint(matmuls[matmuls.size() - 1]);
+
+    // again manual..
+    // TODO: We assume alpha, beta = 1.
+    auto O1 = createAlloc(A2, A3, rewriter, loc);
+    auto O2 = createAlloc(O1, A4, rewriter, loc);
+    auto O3 = createAlloc(O2, A5, rewriter, loc);
+    auto O4 = createAlloc(O3, A6, rewriter, loc);
+    auto O5 = createAlloc(A1, O4, rewriter, loc);
+    createMatmul(alpha1, beta1, A2, A3, O1, rewriter, loc);
+    createMatmul(alpha2, beta2, O1, A4, O2, rewriter, loc);
+    createMatmul(alpha3, beta3, O2, A5, O3, rewriter, loc);
+    createMatmul(alpha4, beta4, O3, A6, O4, rewriter, loc);
+    createMatmul(alpha5, beta5, A1, O4, O5, rewriter, loc);
+
+    // delete all the matmuls and the operations
+    // that create their output buffer.
+    // TODO: Ensure that the alloc operations
+    // do not have any more users before erasing.
+    for (const auto matmul : matmuls)
+      rewriter.eraseOp(matmul);
+    for (const auto alloc : allocs)
+      rewriter.eraseOp(alloc);
+
+    alreadyReordered = true;
+    return success();
+  }
+};
+
+class ChainFiveMatmulOp : public RewritePattern {
+private:
+  static long currentMin;
+  static bool alreadyReordered;
+
+public:
+  ChainFiveMatmulOp(MLIRContext *context)
+      : RewritePattern(MatmulOp::getOperationName(), 0, context) {}
+
+  LogicalResult matchAndRewrite(Operation *op,
+                                PatternRewriter &rewriter) const override {
+
+    if (alreadyReordered)
+      return failure();
+
+    using mlir::matchers::m_Any;
+    using mlir::matchers::m_AnyCapture;
+    Value A1 = nullptr, A2 = nullptr, A3 = nullptr, A4 = nullptr, A5 = nullptr;
+    Value B1 = nullptr, B2 = nullptr, B3 = nullptr;
+    Value alpha1 = nullptr, alpha2 = nullptr, alpha3 = nullptr,
+          alpha4 = nullptr;
+    Value beta1 = nullptr, beta2 = nullptr, beta3 = nullptr, beta4 = nullptr;
+    // clang-format off
+    auto matcher =
+      m_Op<linalg::MatmulOp>(m_AnyCapture(alpha1), m_AnyCapture(beta1), m_AnyCapture(A1), m_AnyCapture(A2),
+        m_Op<linalg::MatmulOp>(m_AnyCapture(alpha2), m_AnyCapture(beta2), m_AnyCapture(B1), m_AnyCapture(A3),
+          m_Op<linalg::MatmulOp>(m_AnyCapture(alpha3), m_AnyCapture(beta3), m_AnyCapture(B2), m_AnyCapture(A4),
+            m_Op<linalg::MatmulOp>(m_AnyCapture(alpha4), m_AnyCapture(beta4), m_AnyCapture(B3), m_AnyCapture(A5), 
+                                   m_Any([](mlir::Value v) { return isNotUsedInMatmulOp(v); } )))));
+    // clang-format on
+    if (!matcher.match(op))
+      return failure();
+
+    SmallVector<Value, 8> capturedAllocs = {A1, A2, A3, A4, A5};
+    auto pVector = getPVector(capturedAllocs);
+    if (!pVector.size())
+      return failure();
+
+    const size_t n = pVector.size();
+    std::vector<std::vector<long>> m(
+        n, std::vector<long>(n, std::numeric_limits<long>::max()));
+    std::vector<std::vector<long>> s(
+        n, std::vector<long>(n, std::numeric_limits<long>::max()));
+    matrixChainOrder(pVector, m, s);
+    if (!m.size() || !s.size())
+      return failure();
+
+    auto currentNumberScalarsMults =
+        getCurrentNumberOfScalarMults({A1, A2, B1, A3, B2, A4, B3, A5});
+
+    LLVM_DEBUG(llvm::dbgs()
+               << "Min number scalar multiplications: " << m[1][n - 1] << "\n");
+    LLVM_DEBUG(llvm::dbgs() << "Current number scalar multiplications: "
+                            << currentNumberScalarsMults << "\n");
+    LLVM_DEBUG(printOptimalParens(s, 1, 5));
+    LLVM_DEBUG(llvm::dbgs() << "\n\n");
+    LLVM_DEBUG(llvm::dbgs() << "Current Min: " << currentMin << "\n");
+
+    if (currentNumberScalarsMults == currentMin) {
+      return failure();
+    }
+    currentMin = m[1][n - 1];
+
+    SmallVector<Operation *, 8> matmuls = {};
+    getMatmuls(op, matmuls);
+    if (matmuls.size() != 4) {
+      LLVM_DEBUG(llvm::dbgs() << "Expect 4 matmul only");
+      return failure();
+    }
+
+    SmallVector<Operation *, 8> allocs = {};
+    getDefiningOpForMatmulOutputBuffer(matmuls, allocs);
+    if (!llvm::all_of(allocs, [](Operation *alloc) {
+          return dyn_cast_or_null<AllocOp>(alloc);
+        })) {
+      LLVM_DEBUG(llvm::dbgs() << "Expect only alloc operations as defining op "
+                                 "for the matmul output buffer\n");
+      return failure();
+    }
+    auto loc = op->getLoc();
+    rewriter.setInsertionPoint(matmuls[matmuls.size() - 1]);
+
+    // again manual..
+    auto O1 = createAlloc(A3, A4, rewriter, loc);
+    auto O2 = createAlloc(A2, O1, rewriter, loc);
+    auto O3 = createAlloc(A1, O2, rewriter, loc);
+    auto O4 = createAlloc(O3, A5, rewriter, loc);
+    createMatmul(alpha1, beta1, A3, A4, O1, rewriter, loc);
+    createMatmul(alpha2, beta2, A2, O1, O2, rewriter, loc);
+    createMatmul(alpha3, beta3, A1, O2, O3, rewriter, loc);
+    createMatmul(alpha4, beta4, O3, A1, O4, rewriter, loc);
+
+    // delete all the matmuls and the operations
+    // that create their output buffer.
+    // TODO: Ensure that the alloc operations
+    // do not have any more users before erasing.
+    for (const auto matmul : matmuls)
+      rewriter.eraseOp(matmul);
+    for (const auto alloc : allocs)
+      rewriter.eraseOp(alloc);
+
+    alreadyReordered = true;
+    return success();
+  }
+};
+
+class ChainFourMatmulOp : public RewritePattern {
+private:
+  static long currentMin;
+  static bool alreadyReordered;
+
+public:
+  ChainFourMatmulOp(MLIRContext *context)
+      : RewritePattern(MatmulOp::getOperationName(), 0, context) {}
+
+  LogicalResult matchAndRewrite(Operation *op,
+                                PatternRewriter &rewriter) const override {
+
+    if (alreadyReordered)
+      return failure();
     //
     // auto module = op->getParentOfType<mlir::ModuleOp>();
     // module.dump();
@@ -147,8 +390,8 @@ public:
     auto matcher = 
       m_Op<linalg::MatmulOp>(m_AnyCapture(alpha1), m_AnyCapture(beta1), m_AnyCapture(A1), m_AnyCapture(A2),
         m_Op<linalg::MatmulOp>(m_AnyCapture(alpha2), m_AnyCapture(beta2), m_AnyCapture(B1), m_AnyCapture(A3),
-          m_Op<linalg::MatmulOp>(m_AnyCapture(alpha3), m_AnyCapture(beta3), 
-                                 m_AnyCapture(B2), m_AnyCapture(A4), m_Any())));
+          m_Op<linalg::MatmulOp>(m_AnyCapture(alpha3), m_AnyCapture(beta3), m_AnyCapture(B2), m_AnyCapture(A4), 
+                                 m_Any([](mlir::Value v) { return isNotUsedInMatmulOp(v); } ))));
     // clang-format on
     if (!matcher.match(op))
       return failure();
@@ -228,14 +471,28 @@ public:
     for (const auto alloc : allocs)
       rewriter.eraseOp(alloc);
 
+    // fire once.
+    alreadyReordered = true;
     return success();
   }
 };
-long ChainMatmulOp::currentMin = std::numeric_limits<long>::max();
+long ChainFourMatmulOp::currentMin = std::numeric_limits<long>::max();
+long ChainFiveMatmulOp::currentMin = std::numeric_limits<long>::max();
+long ChainSixMatmulOp::currentMin = std::numeric_limits<long>::max();
+bool ChainFiveMatmulOp::alreadyReordered = false;
+bool ChainFourMatmulOp::alreadyReordered = false;
+bool ChainSixMatmulOp::alreadyReordered = false;
 
 static void LinalgMatmulChainPassImpl(FuncOp op, MLIRContext *context) {
   OwningRewritePatternList patterns;
-  patterns.insert<ChainMatmulOp>(context);
+  if (chainSize == 4)
+    patterns.insert<ChainFourMatmulOp>(context);
+  else if (chainSize == 5)
+    patterns.insert<ChainFiveMatmulOp>(context);
+  else if (chainSize == 6)
+    patterns.insert<ChainSixMatmulOp>(context);
+  else
+    patterns.insert<ChainFourMatmulOp>(context);
   applyPatternsAndFoldGreedily(op, patterns);
 }
 
